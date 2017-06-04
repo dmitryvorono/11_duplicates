46a10653d732fd5863107a72e67f90816728f553
dvoronov <voronov-d88@yandex.ru>
2017-06-04 12:35:19 +0300

    Refactor find_duplicates

--- duplicates.py
+++ duplicates.py
@@ -1,33 +1,44 @@
 import os
 import pprint
 import argparse
+import collections
 
 
-def find_duplicates(path, saved_path_files={}, duplicates=[]):
+def categorize_files_by_name_and_size(path, saved_path_files=None):
+    if saved_path_files is None:
+        saved_path_files = collections.defaultdict(list)
     if not os.access(path, os.R_OK):
-        return duplicates
+        return None
     with os.scandir(path) as folder_iterator:
         for entry in folder_iterator:
             if entry.is_file():
                 file_information = (entry.name, entry.stat().st_size)
-                if file_information in saved_path_files:
-                    duplicates.append((saved_path_files[file_information],
-                                       entry.path))
-                else:
-                    saved_path_files[file_information] = entry.path
+                saved_path_files[file_information].append(entry.path)
             elif entry.is_dir():
-                find_duplicates(entry.path, saved_path_files)
-    return duplicates
+                categorize_files_by_name_and_size(entry.path, saved_path_files)
+    return saved_path_files
+
+
+def find_duplicates(path):
+    categorized_files = categorize_files_by_name_and_size(path)
+    if categorized_files is None:
+        return None
+    return [categorized_files[key] for key in categorized_files
+            if len(categorized_files[key]) > 1]
 
 
 def print_duplicates(duplicates):
     pprint.pprint(duplicates)
 
 
-if __name__ == '__main__':
+def parse_commandline_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str, nargs='?',
                         help='Path to target folder', default=os.getcwd())
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_commandline_arguments()
     duplicates = find_duplicates(args.path)
     print_duplicates(duplicates)
